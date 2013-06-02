295f6750d47797f4e8feb46bde32efb4bb1e4cc4
Julian Easterling <julian@julianscorner.com>
2013-06-02 01:20:03 -0400

    Added an exception class for an Invalid Distinguished Name to throw during parsing.

---
+++ ToolKit/DirectoryServices/InvalidDistinguishedNameException.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ToolKit.DirectoryServices
+{
+    /// <summary>
+    /// The exception that is thrown when an invalid distinguished name is parsed.
+    /// </summary>
+    [Serializable]
+    public class InvalidDistinguishedNameException : ApplicationException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidDistinguishedNameException"/> class.
+        /// </summary>
+        public InvalidDistinguishedNameException()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidDistinguishedNameException"/> class.
+        /// </summary>
+        /// <param name="message">
+        /// The message.
+        /// </param>
+        public InvalidDistinguishedNameException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidDistinguishedNameException"/> class.
+        /// </summary>
+        /// <param name="message">
+        /// The message.
+        /// </param>
+        /// <param name="innerException">
+        /// The inner exception.
+        /// </param>
+        public InvalidDistinguishedNameException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidDistinguishedNameException"/> class.
+        /// </summary>
+        /// <param name="info">
+        /// The info.
+        /// </param>
+        /// <param name="context">
+        /// The context.
+        /// </param>
+        protected InvalidDistinguishedNameException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
