7c6b685bd1e1ab7475f5f635429399ab71a9fc32
Julian Easterling <julian@julianscorner.com>
2013-10-31 17:58:29 -0400

    Added Disposable Object Class

---
+++ ToolKit/DisposableObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ToolKit
+{
+    /// <summary>
+    /// An abstract class that wraps an object that uses scarce resources and will dispose
+    /// those resource when they are no longer needed.
+    /// </summary>
+    public abstract class DisposableObject : IDisposable
+    {
+    }
+}
