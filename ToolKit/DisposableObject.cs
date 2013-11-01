37df47442086873d89fcda6cfe4d5b382fa221de
Julian Easterling <julian@julianscorner.com>
2013-11-01 12:42:07 -0400

    added unit test and code implementation for disposable objects

--- ToolKit/DisposableObject.cs
+++ ToolKit/DisposableObject.cs
@@ -10,5 +10,64 @@ namespace ToolKit
     /// </summary>
     public abstract class DisposableObject : IDisposable
     {
+        #region -- Fields --
+        private bool _disposed = false;
+        #endregion
+        #region -- Constructors (ctor) --
+        /// <summary>
+        /// Finalizes an instance of the <see cref="DisposableObject"/> class.
+        /// </summary>
+        ~DisposableObject()
+        {
+            Dispose(false);
+        }
+        #endregion
+        #region -- Delegates --
+        #endregion
+        #region -- Events --
+        #endregion
+        #region -- Enumerations --
+        #endregion
+        #region -- Properties --
+        #endregion
+        #region -- Methods: Public --
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources.
+        /// </summary>
+        /// <param name="disposing">
+        /// <c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.
+        /// </param>
+        public virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                DisposeResources(disposing);
+                _disposed = true;
+            }
+        }
+        #endregion
+        #region -- Methods: Protected --
+
+        /// <summary>
+        /// Disposes the resources used by the inherited class.
+        /// </summary>
+        /// <param name="disposing">if set to <c>false</c>, this method has been called by the runtime.</param>
+        protected abstract void DisposeResources(bool disposing);
+        #endregion
+        #region -- Methods: Native (WinAPI) --
+        #endregion
+        #region -- Methods: Private --
+        #endregion
+        #region -- Nested Types --
+        #endregion
     }
 }
