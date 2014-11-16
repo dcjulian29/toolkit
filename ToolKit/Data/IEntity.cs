6a31fcedf0f8cdf8ca3be1ac8621a7e9ebfe0d32
Julian Easterling <julian@julianscorner.com>
2014-11-16 18:07:53 -0500

    Added Entity interface classes

---
+++ ToolKit/Data/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToolKit.Data
+{
+    /// <summary>
+    /// Interface that represents an entity within the data model with
+    /// an integer as its internal ID property
+    /// </summary>
+    public interface IEntity : IEntityWithTypedId<int>
+    {
+    }
+}
---
+++ ToolKit/Data/IEntityWithTypedId.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ToolKit.Data
+{
+    /// <summary>
+    /// This interface serves as the base interface for entities.
+    /// </summary>
+    /// <typeparam name="TId">The type of the id.</typeparam>
+    public interface IEntityWithTypedId<TId>
+        where TId : IEquatable<TId>, IComparable<TId>
+    {
+        /// <summary>
+        /// Gets or sets the internal ID of this entity as persisted by the storage sub-system.
+        /// </summary>
+        TId Id { get; set; }
+
+        /// <summary>
+        /// Determines whether this instance is transient. Transient objects are not
+        /// associated with an item already in storage. More specifically, if the Id
+        /// is the default value, the entity is transient.
+        /// </summary>
+        /// <returns>
+        ///   <c>true</c> if this entity is transient; otherwise, <c>false</c>.
+        /// </returns>
+        bool IsTransient();
+    }
+}
