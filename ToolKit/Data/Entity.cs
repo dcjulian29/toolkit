1cd4f3e0d98a6c693a90732fca952a423d5f380c
Julian Easterling <julian@julianscorner.com>
2014-11-17 15:44:55 -0500

    Added a "default" entity implementation

---
+++ ToolKit/Data/Entity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ToolKit.Data
+{
+    /// <summary>
+    ///  Provides a base class for entities which will be persisted to the database.
+    ///  Include an Id property along with a consistent manner for comparing entities.
+    /// </summary>
+    /// <remarks>
+    /// Since nearly all entities will have a Id type of integer, this 
+    /// class leverages this assumption.  If you want an entity with a Id type
+    /// other than an integer such as string, then
+    /// use <see cref = "EntityWithTypedId{TId}" /> instead.
+    /// </remarks>
+    [Serializable]
+    [DataContract]
+    public abstract class Entity : EntityWithTypedId<int>, IEntity
+    {
+    }
+}
