9e4a76d1b8bb0caed4063cfd30f43b86ade9bbe0
Julian Easterling <julian@julianscorner.com>
2014-11-20 20:34:30 -0500

    Added Enumerable Extensions and Tests

---
+++ UnitTests/Extensions/EnumerableExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace UnitTests.Extensions
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage(
+        "StyleCop.CSharp.DocumentationRules",
+        "SA1600:ElementsMustBeDocumented",
+        Justification = "Test Suites do not need XML Documentation.")]
+    public class EnumerableExtensionsTests
+    {
+        [Fact]
+        public void Each_Should_IterateOverEachItem()
+        {
+            // Arrange
+            IEnumerable<char> enumerable = new[] { 'a', 'b', 'c' };
+            var sb = new StringBuilder();
+
+            // Act
+            enumerable.Each(c => sb.Append(c));
+
+            // Assert
+            Assert.Equal(sb.ToString(), "abc");
+        }
+
+        [Fact]
+        public void Each_Should_ExecuteAction()
+        {
+            // Arrange
+            IEnumerable<char> enumerable = new[] { 'a', 'b', 'c' };
+            var sb = new StringBuilder();
+
+            // Act
+            enumerable.Each(c =>
+            {
+                sb.Append("1");
+                sb.Append("2");
+            });
+
+            // Assert
+            Assert.Equal(sb.ToString(), "121212");
+        }
+    }
+}
