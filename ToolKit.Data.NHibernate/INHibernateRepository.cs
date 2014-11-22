b409ead2040354ab408776bb24f342b45219bb7a
Julian Easterling <julian@julianscorner.com>
2014-11-21 21:46:56 -0500

    Added a base ToolKit Implementation of the Data (Repositories/UnitOfWok) using NHibernate

---
+++ ToolKit.Data.NHibernate/INHibernateRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToolKit.Data.NHibernate
+{
+    /// <summary>
+    /// Interface for a NHibernate Repository supporting the specified entity type
+    /// </summary>
+    /// <typeparam name="T">The entity type this repository supports.</typeparam>
+    /// <typeparam name="TId">The type of the id.</typeparam>
+    public interface INHibernateRepository<T, TId> : IRepository<T, TId>
+        where T : IEntityWithTypedId<TId>
+        where TId : IEquatable<TId>, IComparable<TId>
+    {
+        /// <summary>
+        /// Finds all entities that match the property values.
+        /// </summary>
+        /// <param name="propertyValuePairs">The property value to match.</param>
+        /// <returns>all entities that match the property values.</returns>
+        IEnumerable<T> FindBy(IDictionary<string, object> propertyValuePairs);
+
+        /// <summary>
+        /// Finds the first entity that match the property values if possible.
+        /// </summary>
+        /// <param name="propertyValuePairs">The property value pairs.</param>
+        /// <returns>
+        /// If the entity is found, The first entity that match the property values is returned;
+        /// otherwise, a default entity is returned.
+        /// </returns>
+        T FindFirst(IDictionary<string, object> propertyValuePairs);
+    }
+}
---
+++ ToolKit.Data.NHibernate/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ToolKit.Data.NHibernate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("ToolKit.Data.NHibernate")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c0eb5d17-ae01-4e60-9d05-fb656f0394ce")]
