f54f700cef8d305f182a201676d0e433cb863b6d
Julian Easterling <julian@julianscorner.com>
2015-01-25 22:14:26 -0500

    Added implementation classes for the different persistent engines.

---
+++ ToolKit.Data.NHibernate/SessionFactories/MsSqlCeSessionFactory.cs
@@ -0,0 +1,42 @@
+﻿using FluentNHibernate.Cfg.Db;
+
+namespace ToolKit.Data.NHibernate.SessionFactories
+{
+    /// <summary>
+    /// NHibernate Session Factory using Microsoft SQL Compact
+    /// </summary>
+    public class MsSqlCeSessionFactory : SessionFactoryBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MsSqlCeSessionFactory"/> class.
+        /// </summary>
+        /// <param name="connectionString">The connection string to use.</param>
+        public MsSqlCeSessionFactory(string connectionString)
+            : base(connectionString)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MsSqlCeSessionFactory"/> class.
+        /// </summary>
+        /// <param name="connectionString">The connection string to use.</param>
+        /// <param name="createDatabase">if set to <c>true</c>, create the database.</param>
+        public MsSqlCeSessionFactory(string connectionString, bool createDatabase)
+            : base(connectionString, createDatabase)
+        {
+        }
+
+        /// <summary>
+        /// Gets the object that will configure the persistence layer.
+        /// </summary>
+        public override IPersistenceConfigurer PersistenceConfigurer
+        {
+            get
+            {
+                return MsSqlCeConfiguration.Standard
+                    .Dialect<global::NHibernate.Dialect.MsSqlCe40Dialect>()
+                    .ConnectionString(ConnectionString);
+            }
+        }
+    }
+}
---
+++ ToolKit.Data.NHibernate/SessionFactories/MySqlSessionFactory.cs
@@ -0,0 +1,40 @@
+﻿using FluentNHibernate.Cfg.Db;
+
+namespace ToolKit.Data.NHibernate.SessionFactories
+{
+    /// <summary>
+    /// NHibernate Session Factory using MySQL Server
+    /// </summary>
+    public class MySqlSessionFactory : SessionFactoryBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MySqlSessionFactory"/> class.
+        /// </summary>
+        /// <param name="connectionString">The connection string to use.</param>
+        public MySqlSessionFactory(string connectionString)
+            : base(connectionString)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MySqlSessionFactory"/> class.
+        /// </summary>
+        /// <param name="connectionString">The connection string to use.</param>
+        /// <param name="createDatabase">if set to <c>true</c>, create the database.</param>
+        public MySqlSessionFactory(string connectionString, bool createDatabase)
+            : base(connectionString, createDatabase)
+        {
+        }
+
+        /// <summary>
+        /// Gets the object that will configure the persistence layer.
+        /// </summary>
+        public override IPersistenceConfigurer PersistenceConfigurer
+        {
+            get
+            {
+                return MySQLConfiguration.Standard.ConnectionString(ConnectionString);
+            }
+        }
+    }
+}
