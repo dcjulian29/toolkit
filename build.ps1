05d7ae033b806d7b9c4cb3d2e37a18fb1c9158d6
Julian Easterling <julian@julianscorner.com>
2015-02-08 22:32:01 -0500

    Package task needs to depend on Test task

--- build.ps1
+++ build.ps1
@@ -132,7 +132,7 @@ Task UnitTest -depends Compile, CopySQLiteInterop, xUnit {
     }
 }
 
-Task Package { #-depends Test {
+Task Package -depends Test {
     foreach ($package in (Get-ChildItem -Path $base_directory -Filter "*.nuspec")) {
         exec { 
             nuget.exe pack "$($package.FullName)" -Version $version -Symbols -o "$build_directory"
