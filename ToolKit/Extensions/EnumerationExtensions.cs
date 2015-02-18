195d8967767a28dc6cec66df9778b5afeda98567
Julian Easterling <julian@julianscorner.com>
2015-02-18 12:42:36 -0500

    Added some extenstion methods I have for enumerations

---
+++ ToolKit/Extensions/EnumerationExtensions.cs
@@ -0,0 +1,123 @@
+﻿namespace System
+{
+    /// <summary>
+    /// This class provides extensions to enumerations.
+    /// </summary>
+    public static class EnumerationExtensions
+    {
+        /// <summary>
+        /// Clears a flag within a flag based enumeration.s
+        /// </summary>
+        /// <typeparam name="T">The type of enumeration.</typeparam>
+        /// <param name="target">The target enumeration.</param>
+        /// <param name="value">The value of the type of enumeration.</param>
+        /// <returns>an enumeration where the flag is cleared.</returns>
+        public static T ClearFlag<T>(this Enum target, T value)
+        {
+            if (target.GetType() != value.GetType())
+            {
+                throw new InvalidOperationException("Enumeration Type Mismatch!");
+            }
+
+            try
+            {
+                return (T)(object)((int)(object)target & ~(int)(object)value);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException(
+                  string.Format("Could not remove value from enumerated type '{0}'.", typeof(T).Name),
+                  ex);
+            }
+        }
+
+        /// <summary>
+        /// Determines if the enumeration contains the provided flag enabled.
+        /// </summary>
+        /// <typeparam name="T">The type of enumeration.</typeparam>
+        /// <param name="target">The target enumeration.</param>
+        /// <param name="value">The value of the type of enumeration.</param>
+        /// <returns>
+        ///   <c>true</c> if the enumeration contains the provided flag enabled;
+        ///   otherwise, <c>false</c>.
+        /// </returns>
+        public static bool Contains<T>(this Enum target, T value)
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException("Argument is not an Enumeration!");
+            }
+
+            if (target.GetType() != value.GetType())
+            {
+                throw new InvalidOperationException("Enumeration Type Mismatch!");
+            }
+
+            try
+            {
+                return ((int)(object)target & (int)(object)value) == (int)(object)value;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Determines if the enumeration is the provided enumeration value.
+        /// </summary>
+        /// <typeparam name="T">The type of enumeration.</typeparam>
+        /// <param name="target">The target enumeration.</param>
+        /// <param name="value">The value of the type of enumeration.</param>
+        /// <returns>
+        ///   <c>true</c> if the enumeration is the provided enumeration value; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool Is<T>(this Enum target, T value)
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException("Argument is not an Enumeration!");
+            }
+
+            if (target.GetType() != value.GetType())
+            {
+                throw new InvalidOperationException("Enumeration Type Mismatch!");
+            }
+
+            try
+            {
+                return (int)(object)target == (int)(object)value;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Sets a flag within a flag based enumeration
+        /// </summary>
+        /// <typeparam name="T">The type of enumeration.</typeparam>
+        /// <param name="target">The target enumeration.</param>
+        /// <param name="value">The value of the type of enumeration.</param>
+        /// <returns>an enumeration where the flag is set.</returns>
+        public static T SetFlag<T>(this Enum target, T value)
+        {
+            if (target.GetType() != value.GetType())
+            {
+                throw new InvalidOperationException("Enumeration Type Mismatch!");
+            }
+
+            try
+            {
+                return (T)(object)((int)(object)target | (int)(object)value);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException(
+                  string.Format("Could not append value from enumerated type '{0}'.", typeof(T).Name),
+                  ex);
+            }
+        }
+    }
+}
---
+++ UnitTests/Extensions/EnumerationExtensionsTests.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace UnitTests.Extensions
+{
+    [SuppressMessage(
+        "StyleCop.CSharp.DocumentationRules",
+        "SA1600:ElementsMustBeDocumented",
+        Justification = "Test Suites do not need XML Documentation.")]
+    public class EnumerationExtensionsTests
+    {
+        [Fact]
+        public void ClearFlag_Should_ClearTheFlag_When_FlagExist()
+        {
+            // Arrange            
+            var userFlag = UserFlag.Disabled | UserFlag.LockedOut;
+
+            // Act
+            userFlag = userFlag.ClearFlag(UserFlag.Disabled);
+
+            // Assert
+            Assert.False(userFlag.Contains(UserFlag.Disabled));
+        }
+
+        [Fact]
+        public void ClearFlag_Should_ThrowInvalidOperationException_When_EnumarationFlagDoesNotExist()
+        {
+            // Arrange            
+            var userFlag = UserFlag.Disabled | UserFlag.LockedOut;
+
+            // Act
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                userFlag.ClearFlag(Groups.Domain);
+            });
+        }
+
+        [Fact]
+        public void Contains_Should_ThrowInvalidOperationException_When_EnumerationTypeIsDifferent()
+        {
+            // Arrange            
+            var userFlag = UserFlag.Disabled | UserFlag.LockedOut;
+
+            // Act
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var result = userFlag.Contains(Groups.Domain);
+            });
+        }
+
+        [Fact]
+        public void ClearFlag_Should_ThrowInvalidOperationException_When_ValueOfFlagDoesNotExist()
+        {
+            // Arrange            
+            var userFlag = UserFlag.Disabled | UserFlag.LockedOut;
+
+            // Act
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                userFlag = (UserFlag)userFlag.ClearFlag(0x2000000);
+            });
+        }
+
+        [Fact]
+        public void Contains_Should_ReturnFalse_When_FlagDoesNotExist()
+        {
+            // Arrange            
+            var userFlag = UserFlag.Disabled | UserFlag.LockedOut;
+
+            // Act
+
+            // Assert
+            Assert.False(userFlag.Contains(UserFlag.Password));
+        }
+
+        [Fact]
+        public void Contains_Should_ReturnTrue_When_FlagDoesExist()
+        {
+            // Arrange            
+            var userFlag = UserFlag.Disabled | UserFlag.LockedOut;
+
+            // Act
+
+            // Assert
+            Assert.True(userFlag.Contains(UserFlag.Disabled));
+        }
+
+        [Fact]
+        public void Contains_Should_ThrowArgumentException_When_FlagDoesNotExistsInEnumeration()
+        {
+            // Arrange            
+            var userFlag = UserFlag.Disabled | UserFlag.LockedOut;
+
+            // Act
+
+            // Assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var result = userFlag.Contains("noEnum");
+            });
+        }
+
+        [Fact]
+        public void Is_Should_ReturnFalse_When_ProvidedEnumerationDoesNotEqualEnumeration()
+        {
+            // Arrange            
+            var color = Color.Blue;
+
+            // Act
+
+            // Assert
+            Assert.False(color.Is(Color.Red));
+        }
+
+        [Fact]
+        public void Is_Should_ReturnTrue_When_ProvidedEnumerationDoesEqualEnumeration()
+        {
+            // Arrange            
+            var color = Color.Blue;
+
+            // Act
+
+            // Assert
+            Assert.True(color.Is(Color.Blue));
+        }
+
+        [Fact]
+        public void Is_Should_ThrowInvalidOperationException_When_EnumerationIsNotSameType()
+        {
+            // Arrange            
+            var userFlag = UserFlag.Disabled | UserFlag.LockedOut;
+
+            // Act
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var result = userFlag.Contains(Color.Green);
+            });
+        }
+
+        [Fact]
+        public void Is_Should_ThrowArgumentException_When_EnumerationValueDoesNotExist()
+        {
+            // Arrange            
+            var color = Color.Blue;
+
+            // Act
+
+            // Assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var result = color.Is("-1");
+            });
+        }
+
+        [Fact]
+        public void SetFlag_Should_SetTheFlag_When_FlagExistInEnumeration()
+        {
+            // Arrange            
+            var userFlags = UserFlag.Normal;
+
+            // Act
+            userFlags = userFlags.SetFlag(UserFlag.NotRequired);
+
+            // Assert
+            Assert.True(userFlags.Contains(UserFlag.NotRequired));
+        }
+
+        [Fact]
+        public void SetFlag_Should_ThrowInvalidOperationException_When_EnumerationTypeIsDifferent()
+        {
+            // Arrange            
+            var userFlags = UserFlag.Normal;
+
+            // Act
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                userFlags.SetFlag(Color.Blue);
+            });
+        }
+
+        [Fact]
+        public void SetFlag_Should_ThrowInvalidOperationException_When_ValueOfFlagDoesNotExist()
+        {
+            // Arrange            
+            var userFlags = UserFlag.Normal;
+
+            // Act
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                userFlags.SetFlag("nonenum");
+            });
+        }
+
+        private enum Color
+        {
+            Red,
+            Blue,
+            Green
+        }
+
+        [Flags]
+        private enum UserFlag
+        {
+            Disabled = 0x2,
+            LockedOut = 0x10,
+            Password = 0x20,
+            Encrypted = 0x80,
+            Duplicate = 0x100,
+            Normal = 0x200,
+            Workstation = 0x1000,
+            Trusted = 0x80000,
+            NotRequired = 0x100000,
+            Delagated = 0x1000000
+        }
+
+        [Flags]
+        private enum Groups
+        {
+            Global = 0x00000002,
+            Domain = 0x00000004,
+            Local = 0x00000004,
+            Universal = 0x00000008
+        }
+    }
+}
