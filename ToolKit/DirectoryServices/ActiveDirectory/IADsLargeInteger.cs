c9878dd264c813c52f592741e46049766dace8e4
Julian Easterling <julian@julianscorner.com>
2015-10-15 15:01:14 -0400

    Added an Interface and class to handle 64-bit signed integers that some Active Directory properties use.

---
+++ ToolKit/DirectoryServices/ActiveDirectory/IADsLargeInteger.cs
@@ -0,0 +1,20 @@
+﻿namespace ToolKit.DirectoryServices.ActiveDirectory
+{
+    /// <summary>
+    /// The IADsLargeInteger interface is used to manipulate 64-bit integers of the LargeInteger type.
+    /// </summary>
+    public interface IADsLargeInteger
+    {
+        /// <summary>
+        /// Gets or sets the upper 32 bits of the integer.
+        /// </summary>
+        /// <value>The upper 32 bits of the integer.</value>
+        int HighPart { get; set; }
+
+        /// <summary>
+        /// Gets or sets the lower 32 bits of the integer.
+        /// </summary>
+        /// <value>The lower 32 bits of the integer.</value>
+        int LowPart { get; set; }
+    }
+}
