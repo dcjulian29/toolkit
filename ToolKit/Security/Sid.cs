597ab90a070cfc1ace19423f5e15754194d01e19
Julian Easterling <julian@julianscorner.com>
2015-10-19 11:20:35 -0400

    Added a class and test to provide functions related to security identifiers (SIDs)

---
+++ ToolKit/Security/Sid.cs
@@ -0,0 +1,237 @@
+﻿using System.Security.Principal;
+using Toolkit;
+
+namespace ToolKit.Security
+{
+    /// <summary>
+    /// This class provides functions related to Security Identifiers (SIDs)
+    /// </summary>
+    public class Sid
+    {
+        /// <summary>
+        /// Gets the SID for the builtin administrators group on the machine. This is not the same
+        /// as the builtin Administrator *account*.
+        /// </summary>
+        /// <value>The administrators.</value>
+        public static string Administrators
+        {
+            get
+            {
+                return "S-1-5-32-544";
+            }
+        }
+
+        /// <summary>
+        /// Gets the SID for any user logged on without an identity, for instance via an anonymous
+        /// network session. Note that users logged in using the Builtin Guest account are neither
+        /// authenticated nor anonymous. Available on XP and later.
+        /// </summary>
+        /// <value>The anonymous loggedon user.</value>
+        public static string AnonymousLoggedonUser
+        {
+            get
+            {
+                return "S-1-5-7";
+            }
+        }
+
+        /// <summary>
+        /// Gets the SID for any user recognized by the local machine or by a domain. Note that
+        /// users logged in using the Builtin Guest account are not authenticated. However, members
+        /// of the Guests group with individual accounts on the machine or domain are authenticated.
+        /// </summary>
+        /// <value>The authenticated users.</value>
+        public static string AuthenticatedUsers
+        {
+            get
+            {
+                return "S-1-5-11";
+            }
+        }
+
+        /// <summary>
+        /// Gets the SID for group covering users logging in using the local or domain guest
+        /// account. This is not the same as the built-in Guest *account*.
+        /// </summary>
+        /// <value>The built-in guests.</value>
+        public static string BuiltinGuests
+        {
+            get
+            {
+                return "S-1-5-32-546";
+            }
+        }
+
+        /// <summary>
+        /// Gets the SID for group covering all local user accounts, and users on the domain.
+        /// </summary>
+        /// <value>The builtin user.</value>
+        public static string BuiltinUser
+        {
+            get
+            {
+                return "S-1-5-32-545";
+            }
+        }
+
+        /// <summary>
+        /// Gets the SID for users who initally logged onto the machine "interactively", such as
+        /// local logon and Remote Desktops logon.
+        /// </summary>
+        /// <value>The interactive users.</value>
+        public static string InteractiveUsers
+        {
+            get
+            {
+                return "S-1-5-4";
+            }
+        }
+
+        /// <summary>
+        /// Gets the SID for a predefined account for services that presents user credentials for
+        /// local resources and anonymous credentials for network access. Available on XP and later.
+        /// </summary>
+        /// <value>The local service.</value>
+        public static string LocalService
+        {
+            get
+            {
+                return "S-1-5-19";
+            }
+        }
+
+        /// <summary>
+        /// Gets the SID for users accessing the machine remotely, without interactive desktop
+        /// access (ie, file sharing or RPC calls).
+        /// </summary>
+        /// <value>The network logon user.</value>
+        public static string NetworkLogonUser
+        {
+            get
+            {
+                return "S-1-5-2";
+            }
+        }
+
+        /// <summary>
+        /// Gets the SID for a predefined account for services that presents user credentials for
+        /// local resources and the machine ID for network access. Available on XP and later.
+        /// </summary>
+        /// <value>The network service.</value>
+        public static string NetworkService
+        {
+            get
+            {
+                return "S-1-5-20";
+            }
+        }
+
+        /// <summary>
+        /// Gets the SID for the OS itself (including its user mode components.)
+        /// </summary>
+        /// <value>The system.</value>
+        public static string System
+        {
+            get
+            {
+                return "S-1-5-18";
+            }
+        }
+
+        /// <summary>
+        /// Gets the SID for interactive Users who *initially* logged onto the machine specifically
+        /// via Terminal Services or Remote Desktop.
+        /// </summary>
+        /// <value>The terminal server users.</value>
+        public static string TerminalServerUsers
+        {
+            get
+            {
+                return "S-1-5-13";
+            }
+        }
+
+        /// <summary>
+        /// Gets the SID for prior to Windows XP, this SID covers every session: authenticated,
+        /// anonymous, and the Builtin Guest account. For Windows XP and later, this SID does not
+        /// cover anonymous logon sessions - only authenticated and the Builtin Guest account.
+        /// </summary>
+        /// <value>The world.</value>
+        public static string World
+        {
+            get
+            {
+                return "S-1-1-0";
+            }
+        }
+
+        /// <summary>
+        /// Convert the string SID to binary.
+        /// </summary>
+        /// <param name="stringSid">The string SID.</param>
+        /// <returns>byte array of the SID</returns>
+        public static byte[] ToBinary(string stringSid)
+        {
+            var sid = new SecurityIdentifier(stringSid);
+            var sidBytes = new byte[sid.BinaryLength];
+            sid.GetBinaryForm(sidBytes, 0);
+
+            return sidBytes;
+        }
+
+        /// <summary>
+        /// Convert the binary SID to a string in Hexadecimal format
+        /// </summary>
+        /// <param name="binarySid">The binary SID.</param>
+        /// <returns>string in Hexadecimal format</returns>
+        public static string ToHex(byte[] binarySid)
+        {
+            return HexEncoding.ToString(binarySid);
+        }
+
+        /// <summary>
+        /// Convert the string formated SID to a string in Hexadecimal format
+        /// </summary>
+        /// <param name="stringSid">The string sid.</param>
+        /// <returns>string in Hexadecimal format</returns>
+        public static string ToHex(string stringSid)
+        {
+            return ToHex(ToBinary(stringSid));
+        }
+
+        /// <summary>
+        /// Translates the SID into its name in the DOMAIN\USER format
+        /// </summary>
+        /// <param name="binarySid">The binary SID.</param>
+        /// <returns>string containing the name of the SID</returns>
+        public static string ToName(byte[] binarySid)
+        {
+            return ToName(ToString(binarySid));
+        }
+
+        /// <summary>
+        /// Translates the SID into its name in the DOMAIN\USER format
+        /// </summary>
+        /// <param name="stringSid">The string SID.</param>
+        /// <returns>string containing the name of the SID</returns>
+        public static string ToName(string stringSid)
+        {
+            var sid = new SecurityIdentifier(stringSid);
+            var nac = (NTAccount)sid.Translate(typeof(NTAccount));
+
+            return nac.ToString();
+        }
+
+        /// <summary>
+        /// Convert the binary SID to a string format
+        /// </summary>
+        /// <param name="binarySid">The binary sid.</param>
+        /// <returns>string containing a string representation of the SID</returns>
+        public static string ToString(byte[] binarySid)
+        {
+            var sid = new SecurityIdentifier(binarySid, 0);
+
+            return sid.ToString();
+        }
+    }
+}
---
+++ UnitTests/Security/SidTests.cs
@@ -0,0 +1,254 @@
+﻿using ToolKit.Security;
+using Xunit;
+
+namespace UnitTests.Security
+{
+    public class SidTests
+    {
+        [Fact]
+        public void Administrators_Should_ReturnExpectedResult()
+        {
+            // Arrange
+            var expected = "S-1-5-32-544";
+
+            // Act
+            var actual = Sid.Administrators;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void AnonymousLoggedonUser_Should_ReturnExpectedResult()
+        {
+            // Arrange
+            var expected = "S-1-5-7";
+
+            // Act
+            var actual = Sid.AnonymousLoggedonUser;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void AuthenticatedUsers_Should_ReturnExpectedResult()
+        {
+            // Arrange
+            var expected = "S-1-5-11";
+
+            // Act
+            var actual = Sid.AuthenticatedUsers;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void BuiltinGuests_Should_ReturnExpectedResult()
+        {
+            // Arrange
+            var expected = "S-1-5-32-546";
+
+            // Act
+            var actual = Sid.BuiltinGuests;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void BuiltinUser_Should_ReturnExpectedResult()
+        {
+            // Arrange
+            var expected = "S-1-5-32-545";
+
+            // Act
+            var actual = Sid.BuiltinUser;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void InteractiveUsers_Should_ReturnExpectedResult()
+        {
+            // Arrange
+            var expected = "S-1-5-4";
+
+            // Act
+            var actual = Sid.InteractiveUsers;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LocalService_Should_ReturnExpectedResult()
+        {
+            // Arrange
+            var expected = "S-1-5-19";
+
+            // Act
+            var actual = Sid.LocalService;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void NetworkLogonUser_Should_ReturnExpectedResult()
+        {
+            // Arrange
+            var expected = "S-1-5-2";
+
+            // Act
+            var actual = Sid.NetworkLogonUser;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void NetworkService_Should_ReturnExpectedResult()
+        {
+            // Arrange
+            var expected = "S-1-5-20";
+
+            // Act
+            var actual = Sid.NetworkService;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void System_Should_ReturnExpectedResult()
+        {
+            // Arrange
+            var expected = "S-1-5-18";
+
+            // Act
+            var actual = Sid.System;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TerminalServerUsers_Should_ReturnExpectedResult()
+        {
+            // Arrange
+            var expected = "S-1-5-13";
+
+            // Act
+            var actual = Sid.TerminalServerUsers;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ToBinary_Should_ReturnExpectedResult()
+        {
+            // Arrange
+            var expected = new byte[] { 0x01, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05,
+                0x15, 0x00, 0x00, 0x00, 0x2E, 0x43, 0xAC , 0x40, 0xC0, 0x85, 0x38,
+                0x5D, 0x07, 0xE5, 0x3B, 0x2B };
+            var sid = "S-1-5-21-1085031214-1563985344-725345543";
+
+            // Act
+            var actual = Sid.ToBinary(sid);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ToHex_Should_ReturnExpectedResult_When_ByteProvided()
+        {
+            // Arrange
+            var expected = "0104000000000005150000002E43AC40C085385D07E53B2B";
+            var sid = new byte[] { 0x01, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05,
+                0x15, 0x00, 0x00, 0x00, 0x2E, 0x43, 0xAC , 0x40, 0xC0, 0x85, 0x38,
+                0x5D, 0x07, 0xE5, 0x3B, 0x2B };
+
+            // Act
+            var actual = Sid.ToHex(sid);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ToHex_Should_ReturnExpectedResult_When_StringProvided()
+        {
+            // Arrange
+            var expected = "0104000000000005150000002E43AC40C085385D07E53B2B";
+            var sid = "S-1-5-21-1085031214-1563985344-725345543";
+
+            // Act
+            var actual = Sid.ToHex(sid);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ToName_Should_ReturnExpectedResult_When_ByteProvided()
+        {
+            // Arrange
+            var expected = @"NT AUTHORITY\INTERACTIVE";
+            var sid = new byte[] { 1, 1, 0, 0, 0, 0, 0, 5, 4, 0, 0, 0 };
+
+            // Act
+            var actual = Sid.ToName(sid);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ToName_Should_ReturnExpectedResult_When_StringProvided()
+        {
+            // Arrange
+            var expected = @"NT AUTHORITY\INTERACTIVE";
+            var sid = "S-1-5-4";
+
+            // Act
+            var actual = Sid.ToName(sid);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ToString_Should_ReturnExpectedResult_When_ByteProvided()
+        {
+            // Arrange
+            var expected = "S-1-5-21-1085031214-1563985344-725345543";
+            var sid = new byte[] { 0x01, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05,
+                0x15, 0x00, 0x00, 0x00, 0x2E, 0x43, 0xAC , 0x40, 0xC0, 0x85, 0x38,
+                0x5D, 0x07, 0xE5, 0x3B, 0x2B };
+
+            // Act
+            var actual = Sid.ToString(sid);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void World_Should_ReturnExpectedResult()
+        {
+            // Arrange
+            var expected = "S-1-1-0";
+
+            // Act
+            var actual = Sid.World;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
