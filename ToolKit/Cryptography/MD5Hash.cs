4a8766dfeff4a6ea6c876b0c5930d1b44d3842a4
Julian Easterling <julian@julianscorner.com>
2016-08-30 18:59:09 -0400

    Added MD5 Hash class and unit tests.

---
+++ ToolKit/Cryptography/MD5Hash.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.IO;
+
+namespace ToolKit.Cryptography
+{
+    /// <summary>
+    /// Implementation of MD5 which is a cryptographic hash function that takes as input a message of
+    /// arbitrary length and produces as output a 128-bit "fingerprint" or "message digest" of the
+    /// input. The MD5 algorithm is intended for digital signature applications, where a large file
+    /// must be "compressed" in a secure manner before being encrypted with a private (secret) key
+    /// under a public-key crypto-system such as RSA. (RFC1321)
+    /// </summary>
+    /// <remarks>
+    /// Hash functions are fundamental to modern cryptography. These functions map binary strings of
+    /// an arbitrary length to small binary strings of a fixed length, known as hash values. A
+    /// cryptographic hash function has the property that it is computationally infeasible to find
+    /// two distinct inputs that hash to the same value. Hash functions are commonly used with
+    /// digital signatures and for data integrity.
+    /// </remarks>
+    [Obsolete("The MD5 Hash has known vulnerabilities. You should use a stronger hash algorithm.")]
+
+    // ReSharper disable once InconsistentNaming
+    public class MD5Hash : IHash
+    {
+        private Hash _algorithm = new Hash(Hash.Provider.MD5);
+
+        /// <summary>
+        /// Creates an instance of the Hash Algorithm.
+        /// </summary>
+        /// <returns>an instance of the MD5 Hash object</returns>
+        public static MD5Hash Create()
+        {
+            return new MD5Hash();
+        }
+
+        /// <summary>
+        /// Calculates hash for a stream.
+        /// </summary>
+        /// <param name="data">The data to be used to hash.</param>
+        /// <returns>a string containing the hash of the data provided.</returns>
+        public string Compute(Stream data)
+        {
+            return _algorithm.Calculate(data).Hex;
+        }
+
+        /// <summary>
+        /// Calculates hash for an array of bytes.
+        /// </summary>
+        /// <param name="data">The data to be used to hash.</param>
+        /// <returns>a string containing the hash of the data provided.</returns>
+        public string Compute(byte[] data)
+        {
+            return _algorithm.Calculate(new EncryptionData(data)).Hex;
+        }
+
+        /// <summary>
+        /// Calculates hash for a string.
+        /// </summary>
+        /// <param name="data">The data to be used to hash.</param>
+        /// <returns>a string containing the hash of the data provided.</returns>
+        public string Compute(string data)
+        {
+            return _algorithm.Calculate(new EncryptionData(data)).Hex;
+        }
+
+        /// <summary>
+        /// Calculates hash for data contained within an <see cref="EncryptionData"/>.
+        /// </summary>
+        /// <param name="data">The data to be used to hash.</param>
+        /// <returns>a string containing the hash of the data provided.</returns>
+        public string Compute(EncryptionData data)
+        {
+            return _algorithm.Calculate(data).Hex;
+        }
+
+        /// <summary>
+        /// Calculates hash for data contained within an <see cref="EncryptionData"/> with a prefixed
+        /// salt value contained within an <see cref="EncryptionData"/>. A "salt" is random data
+        /// prefixed to every hashed value to prevent common dictionary attacks.
+        /// </summary>
+        /// <param name="data">The data to be used to hash.</param>
+        /// <param name="salt">The salt to use during the hash.</param>
+        /// <returns>a string containing the hash of the data provided.</returns>
+        public string Compute(EncryptionData data, EncryptionData salt)
+        {
+            return _algorithm.Calculate(data, salt).Hex;
+        }
+
+        /// <summary>
+        /// Calculates hash for a stream.
+        /// </summary>
+        /// <param name="data">The data to be used to hash.</param>
+        /// <returns>a byte array containing the hash of the data provided.</returns>
+        public byte[] ComputeToBytes(Stream data)
+        {
+            return _algorithm.Calculate(data).Bytes;
+        }
+
+        /// <summary>
+        /// Calculates hash for an array of bytes.
+        /// </summary>
+        /// <param name="data">The data to be used to hash.</param>
+        /// <returns>a byte array containing the hash of the data provided.</returns>
+        public byte[] ComputeToBytes(byte[] data)
+        {
+            return _algorithm.Calculate(new EncryptionData(data)).Bytes;
+        }
+
+        /// <summary>
+        /// Calculates hash for a string.
+        /// </summary>
+        /// <param name="data">The data to be used to hash.</param>
+        /// <returns>a byte array containing the hash of the data provided.</returns>
+        public byte[] ComputeToBytes(string data)
+        {
+            return _algorithm.Calculate(new EncryptionData(data)).Bytes;
+        }
+
+        /// <summary>
+        /// Calculates hash for data contained within an <see cref="EncryptionData"/>.
+        /// </summary>
+        /// <param name="data">The data to be used to hash.</param>
+        /// <returns>a byte array containing the hash of the data provided.</returns>
+        public byte[] ComputeToBytes(EncryptionData data)
+        {
+            return _algorithm.Calculate(data).Bytes;
+        }
+
+        /// <summary>
+        /// Calculates hash for data contained within an <see cref="EncryptionData"/> with a prefixed
+        /// salt value contained within an <see cref="EncryptionData"/>. A "salt" is random data
+        /// prefixed to every hashed value to prevent common dictionary attacks.
+        /// </summary>
+        /// <param name="data">The data to be used to hash.</param>
+        /// <param name="salt">The salt to use during the hash.</param>
+        /// <returns>a byte array containing the hash of the data provided.</returns>
+        public byte[] ComputeToBytes(EncryptionData data, EncryptionData salt)
+        {
+            return _algorithm.Calculate(data, salt).Bytes;
+        }
+    }
+}
