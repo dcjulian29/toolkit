1844c45e6c139e7589f954cbe7692b30257d39a2
Julian Easterling <julian@julianscorner.com>
2018-07-11 14:05:44 -0400

    Added DiffieHellman class to provide encrypt and decrypt functions. Wanted to put this in .Net Core project but it is not supported by .Net Standard 2.0 even though it is in .Net Core 2.1... :(

---
+++ UnitTests/Cryptography/DiffieHellmanTests.cs
@@ -0,0 +1,29 @@
+﻿using ToolKit.Cryptography;
+using Xunit;
+
+namespace UnitTests
+{
+    public class DiffieHellmanTests
+    {
+        [Fact]
+        public void Decrypt_Should_EncryptTheExpectedResult()
+        {
+            // Arrange
+            var expected = new EncryptionData("The Secret Key");
+
+            var customerA = new DiffieHellman();
+            var customerB = new DiffieHellman();
+
+            // Customer B wants to talk to Customer A
+            var secret = customerB.Encrypt(customerA.PublicKey, expected);
+            var publicKey = customerB.PublicKey;
+            var iv = customerB.IV;
+
+            // Act
+            var actual = customerA.Decrypt(publicKey, secret, iv);
+
+            // Assert
+            Assert.Equal(expected.Text, actual.Text);
+        }
+    }
+}
