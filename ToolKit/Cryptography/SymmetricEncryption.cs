3a7a58e8e445b7c973f0107d2fad543722931184
Julian Easterling <julian@julianscorner.com>
2020-07-29 14:38:07 -0400

    Removed unnecessary using statement

--- ToolKit/Cryptography/SymmetricEncryption.cs
+++ ToolKit/Cryptography/SymmetricEncryption.cs
@@ -1,8 +1,7 @@
-﻿using System;
+using System;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Security.Cryptography;
-using Common.Logging;
 
 namespace ToolKit.Cryptography
 {
