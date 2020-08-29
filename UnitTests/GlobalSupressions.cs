39192eaa8dedaa73fc8eb2fe6fdcebfda21e8c05
Julian Easterling <julian@julianscorner.com>
2020-08-29 09:31:12 -0400

    Added global supressions for Unit Test project

--- UnitTests/GlobalSupressions.cs
+++ UnitTests/GlobalSupressions.cs
@@ -1,10 +1,10 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage 
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
+// Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 //
-// To add a suppression to this file, right-click the message in the 
-// Code Analysis results, point to "Suppress Message", and click 
+// To add a suppression to this file, right-click the message in the
+// Code Analysis results, point to "Suppress Message", and click
 // "In Suppression File".
 // You do not need to add suppressions to this file manually.
 
@@ -12,3 +12,13 @@
     "Microsoft.Design",
     "CA1016:MarkAssembliesWithAssemblyVersion",
     Justification = "Version Strings are provided by build script.")]
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage(
+    "Microsoft.Naming",
+    "CA1707:IdentifiersShouldNotContainUnderscores",
+    Justification = "My unit tests follow a specific pattern which include underscores.")]
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage(
+    "Microsoft.Design",
+    "CA1034:NestedTypesShouldNotBeVisible",
+    Justification = "Unit Tests tend to break analysis rules.")]
