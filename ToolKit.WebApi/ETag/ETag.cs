eb7ee4ebbcdbd9019a0bf3bacd6be3d2d8501573
Julian Easterling <julian@julianscorner.com>
2020-09-10 13:33:44 -0400

    Added ETag class and enumeration of match types

---
+++ ToolKit.WebApi/ETag/ETag.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ToolKit.WebApi.ETag
+{
+    /// <summary>
+    ///   The ETag or entity tag is one of several mechanisms that HTTP provides for Web cache
+    ///   validation, which allows a client to make conditional requests.
+    /// </summary>
+    public class ETag
+    {
+        /// <summary>
+        ///   Gets or sets the opaque identifier assigned by a Web server to a specific version of a resource.
+        /// </summary>
+        public string Tag { get; set; }
+    }
+}
