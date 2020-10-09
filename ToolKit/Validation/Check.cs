5e0b66d08314b4692920554743dc61d6ee97ff74
Julian Easterling <julian@julianscorner.com>
2020-10-09 11:43:13 -0400

    Added a parameter check to make sure number is NotNegative

--- ToolKit/Validation/Check.cs
+++ ToolKit/Validation/Check.cs
@@ -41,6 +41,39 @@ namespace ToolKit.Validation
         }
 
         /// <summary>
+        /// Check to validate that the number is not negative.
+        /// </summary>
+        /// <param name="value">The parameter's value.</param>
+        /// <param name="parameterName">The parameter name.</param>
+        /// <returns>The value of the parameter if it is not negative.</returns>
+        public static int NotNegative(int value, string parameterName)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException(parameterName);
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Check to validate that the number is not negative.
+        /// </summary>
+        /// <param name="value">The parameter's value.</param>
+        /// <param name="parameterName">The parameter name.</param>
+        /// <param name="message">The exception message to use.</param>
+        /// <returns>The value of the parameter if it is not negative.</returns>
+        public static int NotNegative(int value, string parameterName, string message)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException(message, parameterName);
+            }
+
+            return value;
+        }
+
+        /// <summary>
         /// Check to validate that the parameter is not null.
         /// </summary>
         /// <typeparam name="T">The type of the parameter.</typeparam>
