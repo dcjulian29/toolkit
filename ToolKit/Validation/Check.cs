7c623ca955121f968ac5afc2d3847180dc5e179f
Julian Easterling <julian@julianscorner.com>
2020-10-08 20:29:30 -0400

    Added a validation for providing an exception message when check if argument is null or empty

--- ToolKit/Validation/Check.cs
+++ ToolKit/Validation/Check.cs
@@ -8,6 +8,39 @@ namespace ToolKit.Validation
     public static class Check
     {
         /// <summary>
+        /// Check to validate that the string is not null or empty.
+        /// </summary>
+        /// <param name="value">The parameter's value.</param>
+        /// <param name="parameterName">The parameter's string representation.</param>
+        /// <returns>The value of the parameter if it is not <c>null</c> or empty.</returns>
+        public static string NotEmpty(string value, string parameterName)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException(parameterName);
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Check to validate that the string is not null or empty.
+        /// </summary>
+        /// <param name="value">The parameter's value.</param>
+        /// <param name="parameterName">The parameter's string representation.</param>
+        /// <param name="message">The exception message to use.</param>
+        /// <returns>The value of the parameter if it is not <c>null</c> or empty.</returns>
+        public static string NotEmpty(string value, string parameterName, string message)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException(message, parameterName);
+            }
+
+            return value;
+        }
+
+        /// <summary>
         /// Check to validate that the parameter is not null.
         /// </summary>
         /// <typeparam name="T">The type of the parameter.</typeparam>
@@ -15,10 +48,7 @@ namespace ToolKit.Validation
         /// <param name="parameterName">The parameter's string representation.</param>
         /// <returns>The value of the parameter if it is not <c>null</c>.</returns>
         public static T NotNull<T>(T value, string parameterName)
-            where T : class
-        {
-            return value ?? throw new ArgumentNullException(parameterName);
-        }
+            where T : class => value ?? throw new ArgumentNullException(parameterName);
 
         /// <summary>
         /// Check to validate that the parameter is not null.
@@ -33,22 +63,6 @@ namespace ToolKit.Validation
             if (!value.HasValue)
             {
                 throw new ArgumentNullException(parameterName);
-            }
-
-            return value;
-        }
-
-        /// <summary>
-        /// Check to validate that the string is not null or empty.
-        /// </summary>
-        /// <param name="value">The parameter's value.</param>
-        /// <param name="parameterName">The parameter's string representation.</param>
-        /// <returns>The value of the parameter if it is not <c>null</c> or empty.</returns>
-        public static string NotEmpty(string value, string parameterName)
-        {
-            if (string.IsNullOrWhiteSpace(value))
-            {
-                throw new ArgumentException(parameterName);
             }
 
             return value;
